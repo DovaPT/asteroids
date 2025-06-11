c0bbdb92508386f3c29f7eb75702ae2e9604d87e
DovaPT <peterd5590@gmail.com>
2025-06-11 16:40:10 +0200

    added circle

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(30, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60.0)
 
 
 if __name__ == "__main__":
